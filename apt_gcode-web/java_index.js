284ebda212b0d1f647fd8b540df7815ff761b855
karlo-c277 <karlo.ugrin@gmail.com>
2026-08-23 15:12:06 +0200

    editing background

--- apt_gcode-web/java_index.js
+++ apt_gcode-web/java_index.js
@@ -4,6 +4,7 @@ const costumOptions = document.getElementById("costumOptions");
 const costumOutput = document.getElementById("costumOutput");
 const downloadCheckbox = document.getElementById("downloadOutputCheck");
 const errorBox = document.getElementById("errorBox");
+const root = document.documentElement;
 
 function updateCostumPanels() {
     const isCostum = presetSelect.value === "costum";
@@ -25,3 +26,49 @@ presetSelect.addEventListener(
 downloadCheckbox.addEventListener("change", updateCostumPanels);
 updateCostumPanels();
 
+let mouseX = window.innerWidth / 2;
+let mouseY = window.innerHeight / 2;
+
+let targetX = mouseX;
+let targetY = mouseX;
+
+document.addEventListener("mousemove", (event) => {
+    targetX = event.clientX;
+    targetY = event.clientY;
+});
+
+function animateMouseEffect() {
+    mouseX += (targetX - mouseX) * 0.08;
+    mouseY += (targetY - mouseY) * 0.08;
+
+    root.style.setProperty(
+        "--mouse-x",
+        `${mouseY}px`
+    );
+    root.style.setProperty(
+        "--mouse-y",
+        `${mouseY}px`
+    );
+    requestAnimationFrame(animateMouseEffect);
+}
+animateMouseEffect();
+
+document.addEventListener("mouseover", (event) => {
+    if (event.target.closest("#translateButtton")) {
+        root.style.setProperty("--mouse-color", "0, 180, 50");
+        return;
+    }
+    if (event.target.closest("input, select")) {
+        root.style.setProperty("--mouse-color", "212, 169, 0");
+        return;
+    }
+    if (event.target.closest(".hiddenPanel")) {
+        root.style.setProperty("--mouse-color", "255, 145, 0");
+        return;
+    }
+    if (event.target.closest(".terminal-container")) {
+        root.style.setProperty("--mouse-color", "40, 150, 255");
+        return;
+    }
+    root.style.setProperty("--mouse-color", "0, 180, 50");
+});
